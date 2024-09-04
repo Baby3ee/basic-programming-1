64087768037ae1add1aa0f951018d2e387f77b84
NNAN26 <Chomphunut1100@gmail.com>
2024-09-04 20:45:12 +0700

    Update homework.ts

--- ep6/homework.ts
+++ ep6/homework.ts
@@ -1,5 +1,14 @@
 const me = {
     firstName : "Chomphunut" ,
     lastName: "Phaengya" ,
-    age : 20
+    address : 254 ,
+    Village : 9 ,
+    road : "บางจาอ้อ" ,
+    district : "คลองน้อย" ,
+    prefecture : "พระนครหลวง" ,
+    province : "สระบุรี" ,
+    postcode : "25693" ,
+    telephone : "02-349-120" ,
+    mobilePhone : "083-2543698",
+    eMail : "Chomphu1452@gmail.com "
 }
